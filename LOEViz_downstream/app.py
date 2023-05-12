04e3439e03fc6bd37ba084c60ff25b100ed828c8
Juniper Mills <gblrfgr@gmail.com>
2023-05-11 19:57:04 -0500

    More reformatting

--- LOEViz_downstream/app.py
+++ LOEViz_downstream/app.py
@@ -26,121 +26,111 @@ cyto.load_extra_layouts()
 # starting value is blank and causes error, but app can still run
 # need to find a way to set value to existing status of node when clicked
 ###
-node_status_slider = html.Div(id='slider', hidden=True, children=[
-    dcc.Slider(
-        id='node-status-slider',
-        min=0,
-        max=3,
-        step=1,
-        marks={
-            0: 'behind',
-            1: 'on track',
-            2: 'ahead',
-            3: 'completed'
-        },
-        included=False,
-        updatemode='drag',
-    )])
+node_status_slider = html.Div(
+    id="slider",
+    hidden=True,
+    children=[
+        dcc.Slider(
+            id="node-status-slider",
+            min=0,
+            max=3,
+            step=1,
+            marks={0: "behind", 1: "on track", 2: "ahead", 3: "completed"},
+            included=False,
+            updatemode="drag",
+        )
+    ],
+)
 
 # cytoscape is the network/graph structure for dash
 cytoscape = cyto.Cytoscape(
-    id='cytoscape',
-    layout={
-        'name': 'dagre',
-        'rankDir': 'TB'
-    },
-    style={'width': '100%', 'height': '500px'},
+    id="cytoscape",
+    layout={"name": "dagre", "rankDir": "TB"},
+    style={"width": "100%", "height": "500px"},
     elements=all_elements,
-    stylesheet=stylesheet
+    stylesheet=stylesheet,
 )
 
 # LAYOUT DETERMINES ORGANIZATION OF COMPONENTS ON THE APP
 
 app.layout = html.Div(
-    id='app-container',
+    id="app-container",
     children=[
         # title block on top
         html.Div(
-            id='title-block',
+            id="title-block",
             children=[
-                html.H1('Project Network'),
-                html.P('Click or hover over an objective to see details'),
-                html.P('Click on a node and use the slider to change the status and see downstream effects'),
-                html.Hr()
-            ]
+                html.H1("Project Network"),
+                html.P("Click or hover over an objective to see details"),
+                html.P(
+                    "Click on a node and use the slider to change the status and see downstream effects"
+                ),
+                html.Hr(),
+            ],
         ),
-
         # cytoscape block
-        html.Div(
-            id='cytoscape-block',
-            children=[
-                cytoscape,
-                html.Hr()
-            ]
-        ),
-
+        html.Div(id="cytoscape-block", children=[cytoscape, html.Hr()]),
         # info block
         html.Div(
-            id='info-block',
+            id="info-block",
             children=[
                 # left column displays click data and slider
                 html.Div(
-                    id='left-col',
-                    style={'width': '40%', 'height': '200px', 'float': 'left'},
+                    id="left-col",
+                    style={"width": "40%", "height": "200px", "float": "left"},
                     children=[
-                        html.H4('Click Info'),
-                        html.P(id='cytoscape-tapNodeData-output'),
-                        node_status_slider
-                    ]
+                        html.H4("Click Info"),
+                        html.P(id="cytoscape-tapNodeData-output"),
+                        node_status_slider,
+                    ],
                 ),
                 # right column displays hover data
                 html.Div(
-                    id='right-col',
-                    style={'width': '40%', 'height': '100px', 'float': 'right'},
+                    id="right-col",
+                    style={"width": "40%", "height": "100px", "float": "right"},
                     children=[
-                        html.H4('Hover Info'),
-                        html.P(id='cytoscape-mouseoverNodeData-output')
-                    ]
+                        html.H4("Hover Info"),
+                        html.P(id="cytoscape-mouseoverNodeData-output"),
+                    ],
                 ),
-                html.Hr()
-            ]
-        )
-
-    ])
+                html.Hr(),
+            ],
+        ),
+    ],
+)
 
 
 # DASH APP CALLBACKS
 
+
 # show slider if node selected
-@app.callback(
-    Output('slider', 'hidden'),
-    Input('cytoscape', 'selectedNodeData'))
+@app.callback(Output("slider", "hidden"), Input("cytoscape", "selectedNodeData"))
 def displaySlider(data):
     return False if data else True
 
 
 # update node status based on slider value
-@app.callback(Output('cytoscape', 'elements'),
-              Input('node-status-slider', 'value'),
-              Input('cytoscape', 'elements'),
-              Input('cytoscape', 'selectedNodeData'))
+@app.callback(
+    Output("cytoscape", "elements"),
+    Input("node-status-slider", "value"),
+    Input("cytoscape", "elements"),
+    Input("cytoscape", "selectedNodeData"),
+)
 def updateNodeStatus(value, elements, selected):
     # create map of values to match slider values
-    slider_val_map = {
-        0: 'behind',
-        1: 'on track',
-        2: 'ahead',
-        3: 'completed'}
+    slider_val_map = {0: "behind", 1: "on track", 2: "ahead", 3: "completed"}
 
     # Update only the selected element(s)
     if selected:
         # get ids
-        ids = [nodeData['id'] for nodeData in selected]
+        ids = [nodeData["id"] for nodeData in selected]
         # access node objects
-        selected_nodes = ((i, n) for i, n in enumerate(elements) if n['data']['id'] in ids)
+        selected_nodes = (
+            (i, n) for i, n in enumerate(elements) if n["data"]["id"] in ids
+        )
         # update 'status' property in the node
         for i, node in selected_nodes:
-            elements[i]['data']['status'] = slider_val_map[value]
+            elements[i]["data"]["status"] = slider_val_map[value]
         # propagate downstream effects
         update_downstream_elements(selected, elements)
 
@@ -148,18 +138,22 @@ def updateNodeStatus(value, elements, selected):
 
 
 # display node info when clicked
-@app.callback(Output('cytoscape-tapNodeData-output', 'children'),
-              Input('cytoscape', 'tapNodeData'))
+@app.callback(
+    Output("cytoscape-tapNodeData-output", "children"),
+    Input("cytoscape", "tapNodeData"),
+)
 def displayTapNodeData(data):
     return json.dumps(data, indent=2)
 
 
 # display node info when hovered over
-@app.callback(Output('cytoscape-mouseoverNodeData-output', 'children'),
-              Input('cytoscape', 'mouseoverNodeData'))
+@app.callback(
+    Output("cytoscape-mouseoverNodeData-output", "children"),
+    Input("cytoscape", "mouseoverNodeData"),
+)
 def displayTapEdgeData(data):
     return json.dumps(data, indent=2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(port=1001, debug=True)
--- LOEViz_local/app.py
+++ LOEViz_local/app.py
@@ -17,91 +17,71 @@ cyto.load_extra_layouts()
 # CREATE MORE COMPLEX COMPONENTS
 # node checklist allows user to select nodes and show local dependencies
 node_checklist = dcc.Checklist(
-        id='node_checklist',
-        options=nodes_list,
-        value=nodes_list
-    )
+    id="node_checklist", options=nodes_list, value=nodes_list
+)
 
 # cytoscape is the network/graph structure for dash
 cytoscape = cyto.Cytoscape(
-        id='cytoscape',
-        layout={
-            'name': 'dagre',
-            'rankDir': 'TB'
-        },
-        style={'width': '100%', 'height': '500px'},
-        elements=all_elements,
-        stylesheet=stylesheet
-    )
+    id="cytoscape",
+    layout={"name": "dagre", "rankDir": "TB"},
+    style={"width": "100%", "height": "500px"},
+    elements=all_elements,
+    stylesheet=stylesheet,
+)
 
 # LAYOUT DETERMINES ORGANIZATION OF COMPONENTS ON THE APP
 
 app.layout = html.Div(
-    id='app-container',
+    id="app-container",
     children=[
         # title block on top
         html.Div(
-            id='title-block',
+            id="title-block",
             children=[
-                html.H1('Project Network'),
-                html.P('Click or hover over an objective to see details'),
-                html.P('Select nodes from the checklist to see local network'),
-                html.Hr()
-            ]
+                html.H1("Project Network"),
+                html.P("Click or hover over an objective to see details"),
+                html.P("Select nodes from the checklist to see local network"),
+                html.Hr(),
+            ],
         ),
-
         # menu block for filters, checklist, etc.
-        html.Div(
-            id='menu-block',
-            children=[
-                node_checklist,
-                html.Hr()
-            ]
-        ),
-
+        html.Div(id="menu-block", children=[node_checklist, html.Hr()]),
         # cytoscape block
-        html.Div(
-            id='cytoscape-block',
-            children=[
-                cytoscape,
-                html.Hr()
-            ]
-        ),
-
+        html.Div(id="cytoscape-block", children=[cytoscape, html.Hr()]),
         # info block
         html.Div(
-            id='info-block',
+            id="info-block",
             children=[
                 # left column displays click data and slider
                 html.Div(
-                    id='left-col',
-                    style={'width': '40%', 'height': '200px', 'float': 'left'},
+                    id="left-col",
+                    style={"width": "40%", "height": "200px", "float": "left"},
                     children=[
-                        html.H4('Click Info'),
-                        html.P(id='cytoscape-tapNodeData-output'),
-                    ]
+                        html.H4("Click Info"),
+                        html.P(id="cytoscape-tapNodeData-output"),
+                    ],
                 ),
                 # right column displays hover data
                 html.Div(
-                    id='right-col',
-                    style={'width': '40%', 'height': '100px', 'float': 'right'},
+                    id="right-col",
+                    style={"width": "40%", "height": "100px", "float": "right"},
                     children=[
-                        html.H4('Hover Info'),
-                        html.P(id='cytoscape-mouseoverNodeData-output')
-                    ]
+                        html.H4("Hover Info"),
+                        html.P(id="cytoscape-mouseoverNodeData-output"),
+                    ],
                 ),
-                html.Hr()
-            ]
-        )
-
-    ])
+                html.Hr(),
+            ],
+        ),
+    ],
+)
 
 
 # CALLBACKS
 
+
 # node checklist displays selected nodes + dependencies
-@app.callback(Output('cytoscape', 'elements'),
-              Input('node_checklist', 'value'))
+@app.callback(Output("cytoscape", "elements"), Input("node_checklist", "value"))
 # update cytoscape based on nodes selected from checklist
 def node_filter(node_checklist):
     selected_nodes = []
@@ -109,33 +89,46 @@ def node_filter(node_checklist):
 
     # loop through all selected nodes
     for node_id in node_checklist:
-
         # add selected nodes
         for node in nodes:
-            if node['data']['id'] == node_id:
+            if node["data"]["id"] == node_id:
                 selected_nodes.append(node)
-                loe_node = next(loe for loe in loes if loe['data']['id'] == node['data']['parent'])
+                loe_node = next(
+                    loe for loe in loes if loe["data"]["id"] == node["data"]["parent"]
+                )
                 selected_nodes.append(loe_node)
 
         # add connected edges
         for edge in edges:
-            source_node = edge['data']['source']
-            target_node = edge['data']['target']
+            source_node = edge["data"]["source"]
+            target_node = edge["data"]["target"]
             # add all edges where selected node is source
             if source_node == node_id:
                 selected_edges.append(edge)
                 # add corresponding target to complete edge
-                new_target = next(node for node in nodes if node['data']['id'] == target_node)
+                new_target = next(
+                    node for node in nodes if node["data"]["id"] == target_node
+                )
                 selected_nodes.append(new_target)
-                loe_node = next(loe for loe in loes if loe['data']['id'] == new_target['data']['parent'])
+                loe_node = next(
+                    loe
+                    for loe in loes
+                    if loe["data"]["id"] == new_target["data"]["parent"]
+                )
                 selected_nodes.append(loe_node)
             # add all edges where selected node is target
             if target_node == node_id:
                 selected_edges.append(edge)
                 # add corresponding source to complete edge
-                new_source = next(node for node in nodes if node['data']['id'] == source_node)
+                new_source = next(
+                    node for node in nodes if node["data"]["id"] == source_node
+                )
                 selected_nodes.append(new_source)
-                loe_node = next(loe for loe in loes if loe['data']['id'] == new_source['data']['parent'])
+                loe_node = next(
+                    loe
+                    for loe in loes
+                    if loe["data"]["id"] == new_source["data"]["parent"]
+                )
                 selected_nodes.append(loe_node)
 
     # remove duplicate nodes
@@ -151,18 +144,23 @@ def node_filter(node_checklist):
 
 
 # click element to display info
-@app.callback(Output('cytoscape-tapNodeData-output', 'children'),
-              Input('cytoscape', 'selectedNodeData'))
+@app.callback(
+    Output("cytoscape-tapNodeData-output", "children"),
+    Input("cytoscape", "selectedNodeData"),
+)
 def displayTapNodeData(data):
     return json.dumps(data, indent=2)
 
+
 # mouse over element to display info
-@app.callback(Output('cytoscape-mouseoverNodeData-output', 'children'),
-              Input('cytoscape', 'mouseoverNodeData'))
+@app.callback(
+    Output("cytoscape-mouseoverNodeData-output", "children"),
+    Input("cytoscape", "mouseoverNodeData"),
+)
 def displayHoverNodeData(data):
     if data:
         return json.dumps(data, indent=2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(port=1002, debug=True)
